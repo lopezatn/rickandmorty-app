67943b39d419ae2352027339f500741f1bfe4a3a
lopezatn <agustinlpzb@gmail.com>
2023-05-18 21:53:45 +0200

    form validation

--- src/components/RegistrationForm/Index.jsx
+++ src/components/RegistrationForm/Index.jsx
@@ -1,19 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./Index.css";
 
 const RegistrationForm = () => {
-    
-    return (
-        <form>
-            <h2>Register in to access rick'n'morty app</h2>
-            <label>
-                Name:
-                <input 
-                    type="text"
-                    value=""
-                />
-            </label>
-        </form>
-    );
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name || !email || !password) {
+      alert("please fill in all fields");
+      return;
+    }
+
+    if (!isValidEmail(email)) {
+      alert("Please enter a valid email");
+      return;
+    }
+
+    if (password.length <= 5) {
+      alert("Password must be longer than 5 characters");
+      return;
+    }
+
+    if (!/\d/.test(password)) {
+      alert("Password must contain at least one number");
+      return;
+    }
+
+    console.log("Submitted successfully:", { name, email, password });
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
+  const isValidEmail = (value) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(value);
+  };
+
+  return (
+    <form className="registration-form" onSubmit={handleSubmit}>
+      <h2>Register to access rick'n'morty app</h2>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit">Register</button>
+    </form>
+  );
+};
 
 export default RegistrationForm;
